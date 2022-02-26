3504c3857bd944d8b1f5d042b3593538a0a17edc
yeonlisa <tkzw4869@gmail.com>
2022-02-26 14:19:42 +0900

    List: Add a Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ListScreen from './screens/ListScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/"><div>List view</div></Route>
-        <Route path="/focus"><div>Focus view</div></Route>
+        <Route exact path="/">
+          <ListScreen />
+        </Route>
+        <Route path="/focus">
+          <div>Focus view</div>
+        </Route>
       </Switch>
     </BrowserRouter>
   );
---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,40 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { nanoid } from 'nanoid';
+
+
+type Props = {};
+
+type Task = {
+    id: string;
+    label: string;
+}
+
+const ListScreen: React.FC<Props> = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTaskLabel, setNewTaskLabel] = useState('');
+
+    const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => 
+        setNewTaskLabel(e.target.value);
+
+    const handleNewTaskKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter' && newTaskLabel !== '') {
+            setTasks(tasks => [...tasks, { id: nanoid(), label: newTaskLabel}]);
+            setNewTaskLabel('');
+        }
+    }
+    
+    return (
+        <div>
+            <ul>
+                {tasks.map((task) => <li key={task.id}>{task.label}</li>)}
+            </ul>
+            <input 
+                value={newTaskLabel} 
+                onChange={handleNewTaskLabelChange} 
+                onKeyPress={handleNewTaskKeyPress} 
+            />
+        </div>
+    );
+};
+
+export default ListScreen;
