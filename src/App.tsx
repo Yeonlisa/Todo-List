d5951d86d0da518f9a1f9d393c1e974ca89fd986
yeonlisa <tkzw4869@gmail.com>
2022-02-26 18:02:28 +0900

    Style Tabs and App Layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,78 @@ import ListScreen from './screens/ListScreen';
 import { Task } from './types';
 import useLocalStorage from './hooks/use-local-storage';
 import TaskContext from './contexts/task-store';
+import styled from 'styled-components';
+import { colors, GlobalStyle } from './styles';
+
+const Layout = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 35px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  margin-bottom: 45px;
+`;
+
+const TabButton = styled(NavLink)`
+  align-items: center;
+  background: #02343F;
+  color: #F0EDCC;
+  display: flex;
+  height: 62px;
+  justify-content: center;
+  text-decoration: none;
+  width: 120px;
+
+  &:first-child {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  &.active {
+    background: ${colors.primary};
+    color: #02343F;
+  }
+`;
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>('tasks', []);
 
   return (
-    <BrowserRouter>
-      <TaskContext.Provider value={[tasks, setTasks]}>
-        <nav>
-          <NavLink exact to="/" activeStyle={{ fontWeight: 'bold' }}>
-            List
-          </NavLink>{' '}
-          -{' '}
-          <NavLink to="/focus" activeStyle={{ fontWeight: 'bold' }}>
-            Focus
-          </NavLink>
-        </nav>
-        <br />
-        <Switch>
-          <Route exact path="/">
-            <ListScreen />
-          </Route>
-          <Route path="/focus">
-            <FocusScreen />
-          </Route>
-        </Switch>
-      </TaskContext.Provider>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <TaskContext.Provider value={[tasks, setTasks]}>
+          <Layout>
+            <Nav>
+              <TabButton exact to="/" activeClassName="active">
+                List
+              </TabButton>
+              <TabButton to="/focus" activeClassName="active">
+                Focus
+              </TabButton>
+            </Nav>
+            <br />
+            
+            <Switch>
+              <Route exact path="/">
+                <ListScreen />
+              </Route>
+              <Route path="/focus">
+                <FocusScreen />
+              </Route>
+            </Switch>
+          </Layout>
+        </TaskContext.Provider>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+export const colors = {
+    primary: "#F0EDCC",
+}
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background: #10414C;
+        color: #F0EDCC;
+    }
+`;
