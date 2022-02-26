2a24e1dd4184d1de0cd039a59bc91eb5602fc6f5
yeonlisa <tkzw4869@gmail.com>
2022-02-26 13:50:23 +0900

    React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/"><div>List view</div></Route>
+        <Route path="/focus"><div>Focus view</div></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
